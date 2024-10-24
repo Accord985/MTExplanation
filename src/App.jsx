bb01ddd79d82815d78575346cf30d97c273eda80
Bobby W <sw785@uw.edu>
2024-10-24 13:59:06 -0700

    added FBD and support for latex

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import q1 from '/q1.svg'
 import './App.css'
 
+import katex from 'katex';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let formula = katex.renderToString("c=\\pm\\sqrt{a^2+b^2}", {throwOnError: false});
 
   return (
     <>
@@ -16,6 +20,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <section>
+        <div id="overlay">
+          <img src="/q1-ground.svg" alt="Question 1 ground" />
+        </div>
+        <img src={q1} alt="Question 1 figure" />
+      </section>
+      <article dangerouslySetInnerHTML={{__html: formula}}></article>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
